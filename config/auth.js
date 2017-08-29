d6da09b33ddb1fc3dbac3a45806c6dcb058a6118
musicteachj <jimilittle3@hotmail.com>
2017-08-29 18:59:22 -0400

    add
    add

---
+++ config/auth.js
@@ -0,0 +1,14 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID': '831299497033010',
+		'clientSecret': '139ea5647987bc2c208672b400d64a50',
+		'profileFields': ['id', 'displayName', 'email'],
+		'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+	},
+
+	'googleAuth' : {
+		'clientID': 'enter client id here',
+		'clientSecret': 'enter client secret here',
+		'callbackURL': 'enter callback here'
+	}
+}
